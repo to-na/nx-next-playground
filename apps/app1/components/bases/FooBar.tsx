7c8940737c540ba93624ea4d845a5acb18033b6c
to-na <tonanatz@gmail.com>
2022-04-09 19:04:28 +0900

    generate

---
+++ apps/app1/components/bases/FooBar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { styled } from '@compiled/react';
+
+type FooBarProps = {
+  prop1: string;
+};
+type FooBarDomProps = {
+  prop1: string;
+};
+
+const FooBarStyle = styled.div``;
+
+const FooBarDom: React.VFC<FooBarDomProps> = (props) => (
+  <FooBarStyle>Code Presentational Component</FooBarStyle>
+);
+
+const FooBar: React.VFC<FooBarProps> = (props) => {
+  // Code Container Logic
+  const domProps = props;
+  return <FooBarDom {...domProps}>Code DOM layer</FooBarDom>;
+};
+
+export default FooBar;
